39ff8e4fb518f2648f9b206b4ee945d12af9c594
alwafor <alwafor1@gmail.com>
2022-11-15 23:09:20 +0300

    add basic card form

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -2,9 +2,17 @@ import PricingCard from '../ui/PricingCard';
 import styles from './App.module.css';
 
 const App = () => {
-  return <div className={styles.container}> 
-    <PricingCard />
-  </div>;
+  return (
+    <div className={styles.container}>
+      <PricingCard
+        description="some description"
+        items={[{ active: true, text: 'item' }]}
+        onClick={() => console.log('hello')}
+        price={25}
+        title="Professional"
+      />
+    </div>
+  );
 };
 
 export default App;
--- src/components/ui/PricingCard/PricingCard.tsx
+++ src/components/ui/PricingCard/PricingCard.tsx
@@ -1,9 +1,55 @@
 import styles from './PricingCard.module.css';
 
-const PricingCard = () => {
-  return (
-    <div className={styles.card}>PricingCard</div>
-  )
+interface Item {
+  active: boolean;
+  text: string;
 }
 
-export default PricingCard
+interface Props {
+  title: string;
+  description: string;
+  price: number;
+  perTime?: string;
+
+  items: Item[];
+
+  buttonText?: string;
+  onClick: (e: React.MouseEvent) => void;
+
+  variant?: 'white' | 'blue';
+}
+
+const PricingCard = ({
+  title,
+  description,
+  price,
+  perTime = 'month',
+  items,
+  buttonText = 'Get Started Now',
+  onClick,
+  variant = 'white',
+}: Props) => {
+  return (
+    <div className={styles.card}>
+      <div className={styles.title}>{title}</div>
+      <div className={styles.description}>{description}</div>
+      <div className={styles.priceWrapper}>
+        <div className={styles.price}>{price}</div>
+        <div className={styles.perTime}> / {perTime}</div>
+      </div>
+
+      <button onClick={onClick}>{buttonText}</button>
+
+      <div className={styles.itemsWrapper}>
+        {items.map((item) => (
+          <div key={item.text} className={styles.item}>
+            <div className={styles.itemStatus}>{item.active ? '+' : '-'}</div>
+            <div className={styles.itemText}>{item.text}</div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default PricingCard;
