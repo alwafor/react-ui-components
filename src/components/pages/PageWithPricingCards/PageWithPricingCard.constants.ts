f5ed929f2b84ac0e453a17e327a416474731229e
AleksanderAleksandrovich <aleksveles228@gmail.com>
2022-11-20 22:41:04 +0300

    modify PricingCards

--- src/components/pages/PageWithPricingCards/PageWithPricingCard.constants.ts
+++ src/components/pages/PageWithPricingCards/PageWithPricingCard.constants.ts
@@ -22,11 +22,72 @@ export const cardsItems: PricingCardItem[] = [
     text: 'Upload graphics & video in up to 4k',
     gray: true,
   },
-  { iconType: 'cross', text: 'Unlimited Projects' },
+  { iconType: 'cross', text: 'Unlimited Projects' ,gray: true,},
   {
     iconType: 'cross',
     text: 'Instant Access to our design system',
     gray: true,
   },
-  { iconType: 'cross', text: 'Create teams to collaborate on designs' },
+  { iconType: 'cross', text: 'Create teams to collaborate on designs' ,gray: true,},
+];
+
+export const cardsItems2: PricingCardItem[] = [
+  {
+    iconType: 'check',
+    text: '20,000+ of PNG & SVG graphics',
+    white: true,
+  },
+  {
+    iconType: 'check',
+    text: 'Access to 100 million stock images',
+    white: true,
+  },
+  {
+    iconType: 'check',
+    text: 'Upload custom icons and fonts',
+    white: true,
+  },
+  { iconType: 'check', text: 'Unlimited Sharing', white: true },
+  {
+    iconType: 'check',
+    text: 'Upload graphics & video in up to 4k',
+    white: true,
+  },
+  { iconType: 'check', text: 'Unlimited Projects', white: true },
+  {
+    iconType: 'cross',
+    text: 'Instant Access to our design system',
+    white: true,
+  },
+  {
+    iconType: 'cross',
+    text: 'Create teams to collaborate on designs',
+    white: true,
+  },
+];
+
+export const cardsItems3: PricingCardItem[] = [
+  {
+    iconType: 'check',
+    text: '20,000+ of PNG & SVG graphics',
+  },
+  {
+    iconType: 'check',
+    text: 'Access to 100 million stock images',
+  },
+  {
+    iconType: 'check',
+    text: 'Upload custom icons and fonts',
+  },
+  { iconType: 'check', text: 'Unlimited Sharing' },
+  {
+    iconType: 'check',
+    text: 'Upload graphics & video in up to 4k',
+  },
+  { iconType: 'check', text: 'Unlimited Projects' },
+  {
+    iconType: 'check',
+    text: 'Instant Access to our design system',
+  },
+  { iconType: 'check', text: 'Create teams to collaborate on designs' },
 ];
--- src/components/pages/PageWithPricingCards/PageWithPricingCards.tsx
+++ src/components/pages/PageWithPricingCards/PageWithPricingCards.tsx
@@ -1,31 +1,32 @@
 import React from 'react';
 import PricingCard from 'src/components/ui/PricingCard';
-import { cardsItems } from './PageWithPricingCard.constants';
+import { cardsItems, cardsItems2, cardsItems3} from './PageWithPricingCard.constants';
 import styles from './PageWithPricingCards.module.css';
 
 const PageWithPricingCards = () => {
   return (
     <div className={styles.wrapper}>
       <PricingCard
-        description=""
+        description="Amet minim mollit non deserunt ullamco est sit aliqua dolor."
         items={cardsItems}
+        onClick={() => console.log('hello')}
+        price={0}
+        title="Freebie"
+      />
+      <PricingCard
+        variant='blue'
+        description="Amet minim mollit non deserunt ullamco est sit aliqua dolor."
+        items={cardsItems2}
         onClick={() => console.log('hello')}
         price={25}
         title="Professional"
       />
       <PricingCard
-        description=""
-        items={cardsItems}
+        description="Amet minim mollit non deserunt ullamco est sit aliqua dolor."
+        items={cardsItems3}
         onClick={() => console.log('hello')}
-        price={25}
-        title="Professional"
-      />
-      <PricingCard
-        description=""
-        items={cardsItems}
-        onClick={() => console.log('hello')}
-        price={25}
-        title="Professional"
+        price={100}
+        title="Enterprise"
       />
     </div>
   );
