f7cfcf42b89bfce3b2a32e9e86d03a380c74d786
alwafor <alwafor1@gmail.com>
2022-11-22 22:16:39 +0300

    add blue variant for PricingCard

--- src/components/ui/PricingCard/PricingCard.tsx
+++ src/components/ui/PricingCard/PricingCard.tsx
@@ -5,22 +5,39 @@ import styles from './PricingCard.module.css';
 import globalStyles from 'src/assets/styles/Global.module.css';
 import { PricingCardProps } from './PricingCard.interfaces';
 
+type StylesKey = keyof typeof styles;
+
+type StylesVariantClasses =
+  | 'card'
+  | 'title'
+  | 'description'
+  | 'price'
+  | 'perTime';
+
 const getIconVariantForCard = (
   cardVariant: 'white' | 'blue',
   iconType: 'check' | 'cross',
 ) => {
   if (cardVariant === 'blue') {
-    return ('white' + iconType[0].toUpperCase() + iconType.slice(1)) as StatusIconVariant;
+    return ('white' +
+      iconType[0].toUpperCase() +
+      iconType.slice(1)) as StatusIconVariant;
   } else {
     if (iconType === 'cross') return 'grayCross';
     else return 'blueCheck';
   }
 };
-const getStyleVariant = (cardVariant: 'white' | 'blue', selectorStyle: string) => {
+
+function getStyleVariant(
+  cardVariant: 'white' | 'blue',
+  selectorStyle: StylesVariantClasses,
+): StylesKey {
   if (cardVariant === 'white') {
-    return selectorStyle ;
+    return selectorStyle as StylesKey;
   } else {
-    return (selectorStyle + cardVariant[0].toUpperCase() + cardVariant.slice(1))
+    return (selectorStyle +
+      cardVariant[0].toUpperCase() +
+      cardVariant.slice(1)) as StylesKey;
   }
 }
 
@@ -38,10 +55,16 @@ const PricingCard = ({
   return (
     <div className={clsx(styles[getStyleVariant(variant, 'card')], className)}>
       <div className={styles[getStyleVariant(variant, 'title')]}>{title}</div>
-      <div className={styles[getStyleVariant(variant, 'description')]}>{description}</div>
+      <div className={styles[getStyleVariant(variant, 'description')]}>
+        {description}
+      </div>
       <div className={styles.priceWrapper}>
-        <div className={styles[getStyleVariant(variant, 'price')]}>${price}</div>
-        <div className={styles[getStyleVariant(variant, 'perTime')]}> / {perTime}</div>
+        <div className={styles[getStyleVariant(variant, 'price')]}>
+          ${price}
+        </div>
+        <div className={styles[getStyleVariant(variant, 'perTime')]}>
+          / {perTime}
+        </div>
       </div>
 
       <button className={styles.button} onClick={onClick}>
@@ -56,6 +79,7 @@ const PricingCard = ({
           >
             <span
               className={clsx(
+                styles.elementSpan,
                 item.bold && globalStyles.medium,
                 item.gray && globalStyles.gray,
                 item.white && globalStyles.white,
